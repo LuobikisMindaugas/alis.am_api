08e6007e3728eae4caa947a3ee26b41211739825
mindaugas <m.luobikis@gmail.com>
2021-06-09 14:31:55 +0100

    entity not found exception handlers

--- src/main/java/lt/codeacademy/alis/api/Endpoint.java
+++ src/main/java/lt/codeacademy/alis/api/Endpoint.java
@@ -6,15 +6,11 @@ package lt.codeacademy.alis.api;
  */
 public interface Endpoint {
     String UUID = "uuid";
-    String NR = "nr";
 
     String API_ROOT = "/alis/api";
 
     String LICENSES = "/licenses";
     String LICENSE = "/{" + UUID + "}";
-    String LICENSE_NR = "/{" + NR + "}";
-    String DELETE_LICENSE = "/{" + UUID + "}/delete";
-
 
     String AREAS = "/areas";
     String AREA = "/{" + UUID + "}";
